cc85bde4a94b15ea13481199fae1bebf460733d9
SeanCox <sean.cox@mac.com>
2016-11-12 02:16:35 -0800

    [setup] serves up static html page

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const port = 1337
+app.listen(port, ()=>{
+  console.log(`jobFilter listening on ${port}`)
+})
