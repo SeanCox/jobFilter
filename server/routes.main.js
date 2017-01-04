cbefca260637868f5fa2c46f33eb2ffc7d809922
SeanCox <sean.cox@mac.com>
2017-01-04 05:07:39 -0800

    [test] built out test schema for jobs

--- server/routes.main.js
+++ server/routes.main.js
@@ -12,4 +12,44 @@ router.use((req, res) => {
 	res.status(404).send('Sorry that does not exist')
 })
 
+// Start mongoose test
+const keys = require('./_keys')
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://${keys.dbUsername}:${keys.dbPassword}@ds133438.mlab.com:33438/jobfilter`)
+
+const db = mongoose.connection
+db.on('error', console.error.bind('connection error:'))
+db.once('open', () => {
+	console.log('database connection established')
+})
+
+const Schema = mongoose.Schema
+
+const jobSchema = new Schema({
+	title: String,
+	company: String,
+	city: String,
+	skills: Array,
+	exp: Number
+})
+
+const Job = mongoose.model('Job', jobSchema)
+
+const testJob = new Job({
+	title: 'Test Job',
+	company: 'test corp',
+	city: 'Los Angeles',
+	skills: ['javascript', 'css', 'html'],
+	exp: 1
+})
+
+testJob.save((err, job) => {
+	if(err) return console.log(err)
+	console.log(`saved test job`)
+})
+
+
+
+
+// End mongoose test
 module.exports = router
