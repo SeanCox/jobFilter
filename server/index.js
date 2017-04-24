9334bb4667b8e2e919ab77b81ec689082cbcde8c
Sean Cox <seancox@Seans-Mac-Pro.local>
2017-04-23 17:03:51 -0700

    heroku fixes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const routesMain = require('./routes.main.js')
 
 app.use('/', routesMain)
 
-const port = 1337
+const port =  process.env.PORT || 1337
 app.listen(port, ()=>{
   console.log(`jobFilter listening on ${port}`)
 })
